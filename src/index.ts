475d27d739dad87dcdf8e3b22f1f0b99e934fd3f
laubernal <lbernalrodrguez@gmail.com>
2021-10-01 12:18:15 +0200

    setting up express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello');
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response): void => {
+  res.send('Hello');
+});
+
+try {
+  app.listen(port, () => {
+    console.log(`Connected successfully to port ${port}`);
+  });
+} catch (error: any) {
+  console.error(`Error occured: ${error.message}`);
+}
