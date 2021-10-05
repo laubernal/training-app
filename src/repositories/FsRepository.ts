337002b57aa4d7d2cf30329c1b7e9eabadf0bce8
laubernal <lbernalrodrguez@gmail.com>
2021-10-05 11:43:01 +0200

    implementing new methods to the fs repository

--- src/repositories/FsRepository.ts
+++ src/repositories/FsRepository.ts
@@ -5,7 +5,9 @@ import { DATA_DIR } from '../constants';
 import { IReader } from '../interfaces/IReader';
 import { IMapper } from '../mappers/IMapper';
 
-export abstract class FsRepository<T, K> {
+type ObjectDefinition = { [key: string]: string };
+
+export abstract class FsRepository<T, K extends ObjectDefinition> {
   protected abstract mapper: IMapper<T, K>;
 
   constructor(private reader: IReader<T>) {}
@@ -18,14 +20,39 @@ export abstract class FsRepository<T, K> {
   }
 
   public save(): void {
-    fs.writeFileSync(path.join(DATA_DIR, this.reader.filename), JSON.stringify(this.reader.data, null, 2));
+    fs.writeFileSync(
+      path.join(DATA_DIR, this.reader.filename),
+      JSON.stringify(this.reader.data, null, 2)
+    );
   }
 
-  public getOne() {}
+  public getOneBy(propName: keyof K, value: string): K {
+    const item = this.reader.data.find((item: T) => {
+      const domainItem = this.mapper.toDomain(item);
+      return domainItem[propName] === value;
+    });
 
-  public getOneBy() {}
+    if (!item) {
+      throw new Error('Item not found');
+    }
 
-  public getAllBy() {}
+    return this.mapper.toDomain(item);
+  }
+
+  public getAllBy(propName: keyof K, value: string): K[] {
+    const items = this.reader.data.filter((item: T) => {
+      const domainItem = this.mapper.toDomain(item);
+      return domainItem[propName] === value;
+    });
+
+    if (!items) {
+      throw new Error('Items not found');
+    }
+
+    return items.map((item: T) => {
+      return this.mapper.toDomain(item);
+    });
+  }
 
   public update() {}
 }
