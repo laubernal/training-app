0c9ec56e204fa99f6cd263ddfa4523e0c8d167d4
laubernal <lbernalrodrguez@gmail.com>
2021-10-13 17:59:57 +0200

    validating strength of the password

--- src/Domain/vo/Password.ts
+++ src/Domain/vo/Password.ts
@@ -8,6 +8,20 @@ const lowerCaseRegex = /^[a-z]$/;
 const numberRegex = /^[0-9]$/;
 const symbolRegex = /^[-#!$@%^&*()_+|~=`{}\[\]:";'<>?,.\/ ]$/;
 
+const defaultOptions: ObjectDefinition = {
+  minLength: 8,
+  minLowercase: 1,
+  minUppercase: 1,
+  minNumbers: 1,
+  minSymbols: 1,
+  pointsPerUnique: 1,
+  pointsPerRepeat: 0.5,
+  pointsForContainingLower: 10,
+  pointsForContainingUpper: 10,
+  pointsForContainingNumber: 10,
+  pointsForContainingSymbol: 10,
+};
+
 export class Password extends VO {
   constructor(private password: string) {
     super();
@@ -22,10 +36,11 @@ export class Password extends VO {
   private validate(): void {
     this.password = this.password.trim();
     this.isEmpty(this.password);
-
-    if (this.password.length < 8) {
-      throw new Error('Password length must be greater than 8');
+    const strength = this.isStrong(this.password);
+    if (strength < 40) {
+      throw new Error('Password must be stronger');
     }
+    console.log(strength);
   }
 
   private hashPassword(password: string): string {
@@ -46,26 +61,74 @@ export class Password extends VO {
         count[character] = 1;
       }
     });
+
     return count;
   }
 
   private analyzePassword(password: string): Object {
     let characterMap: ObjectDefinition = this.countCharacters(password);
     let analysis = {
-      length: characterMap,
+      length: password.length,
       uniqueCharacters: Object.keys(characterMap).length,
       upperCaseCount: 0,
       lowerCaseCount: 0,
       numberCount: 0,
-      symbolCount: 0
+      symbolCount: 0,
     };
 
-    Object.keys(characterMap).forEach((character: string) =>{
+    Object.keys(characterMap).forEach((character: string) => {
       if (upperCaseRegex.test(character)) {
         analysis.upperCaseCount += characterMap[character];
       }
+      if (lowerCaseRegex.test(character)) {
+        analysis.lowerCaseCount += characterMap[character];        
+      }
+      if (numberRegex.test(character)) {
+        analysis.numberCount += characterMap[character];
+      }
+      if (symbolRegex.test(character)) {
+        analysis.symbolCount += characterMap[character];
+      }
     });
-    
+
     return analysis;
+  }
+
+  private scorePassword(analysis: ObjectDefinition, scoringOptions: ObjectDefinition): number {
+    let points = 0;
+
+    points = points + analysis.uniqueCharacters * scoringOptions.pointsPerUnique;
+    points += (analysis.length - analysis.uniqueCharacters) * scoringOptions.pointsPerRepeat;
+
+    if (analysis.upperCaseCount > 0) {
+      points += scoringOptions.pointsForContainingUpper;
+    }
+    if (analysis.lowerCaseCount > 0) {
+      points += scoringOptions.pointsForContainingLower;
+    }
+    if (analysis.numberCount > 0) {
+      points += scoringOptions.pointsForContainingNumber;
+    }
+    if (analysis.symbolCount > 0) {
+      points += scoringOptions.pointsForContainingSymbol;
+    }
+
+    return points;
+  }
+
+  private isStrong(password: string): number {
+    const analysis: ObjectDefinition = this.analyzePassword(password);
+
+    const score = this.scorePassword(analysis, defaultOptions);
+
+    return score;
+
+    // return (
+    //   analysis.length >= defaultOptions.minLength &&
+    //   analysis.lowercaseCount >= defaultOptions.minLowercase &&
+    //   analysis.uppercaseCount >= defaultOptions.minUppercase &&
+    //   analysis.numberCount >= defaultOptions.minNumbers &&
+    //   analysis.symbolCount >= defaultOptions.minSymbols
+    // );
   }
 }
