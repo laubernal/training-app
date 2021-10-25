460705c740d536517f5bf45128a223a625d12b7b
laubernal <lbernalrodrguez@gmail.com>
2021-10-25 16:21:04 +0200

    fixing change of name and imports issue

--- src/Infrastructure/repositories/TrainingRepository.ts
+++ src/Infrastructure/repositories/TrainingRepository.ts
@@ -1,14 +1,14 @@
-import { TRAININGS_JSON } from "../../constants";
-import { Training } from "../../Domain/entities/Training";
-import { ITraining } from "../../Domain/interfaces/ITraining";
-import { TrainingsMapper } from "../mappers/TrainingsMapper";
-import { FsRepository } from "./FsRepository";
-import { JsonFileReader } from "./JsonFileReader";
+import { TRAININGS_JSON } from '../../constants';
+import { Training } from '../../Domain/entities/Training';
+import { TrainingModel } from '../../Domain/interfaces/TrainingModel';
+import { TrainingsMapper } from '../mappers/TrainingsMapper';
+import { FsRepository } from './FsRepository';
+import { JsonFileReader } from './JsonFileReader';
 
-export class TrainingRepository extends FsRepository<ITraining, Training> {
-    protected mapper = new TrainingsMapper();
-    
-    constructor() {
-        super(new JsonFileReader(TRAININGS_JSON));
-    }
+export class TrainingRepository extends FsRepository<TrainingModel, Training> {
+  protected mapper = new TrainingsMapper();
+
+  constructor() {
+    super(new JsonFileReader(TRAININGS_JSON));
+  }
 }
