af515dafa7f513013ff4ac63f831ed1c6a84d367
laubernal <lbernalrodrguez@gmail.com>
2021-10-26 16:59:24 +0200

    starting implementation of validation date

--- src/Domain/vo/TrainingDate.ts
+++ src/Domain/vo/TrainingDate.ts
@@ -4,7 +4,16 @@ import customParseFormat from 'dayjs/plugin/customParseFormat';
 export class TrainingDate {
   public static generate(date: string): string {
     dayjs.extend(customParseFormat);
+    const trainingDate = dayjs(date, 'DD-MM-YYYY', true);
+    console.log(date);
+    console.log(trainingDate);
 
-    return dayjs(date, 'DD-MM-YYYY').format('DD-MM-YYYY');
+    console.log(trainingDate.isValid());
+
+    if (trainingDate.isValid() === false) {
+      throw new Error('Invalid date');
+    }
+
+    return trainingDate.format('DD-MM-YYYY');
   }
 }
