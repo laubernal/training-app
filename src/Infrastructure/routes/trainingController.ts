f765e16ae541297298d013a7859624ea24187b4a
laubernal <lbernalrodrguez@gmail.com>
2021-11-11 16:56:50 +0100

    creting get route decorator

--- src/Infrastructure/routes/trainingController.ts
+++ src/Infrastructure/routes/trainingController.ts
@@ -2,7 +2,8 @@ import express, { Request, Response, Router } from 'express';
 
 import { GetTrainingUseCase } from '../../Application/UseCases/TrainingUseCase/GetTrainingUseCase';
 import { NewTrainingUseCase } from '../../Application/UseCases/TrainingUseCase/NewTrainingUseCase';
-import { logError } from '../decorators/logError';
+import { logError } from '../controllers/decorators/logError';
+import { get } from '../controllers/decorators/routes';
 import { requireAuth } from '../middlewares/requireAuth';
 import { TrainingRepository } from '../repositories/TrainingRepository';
 
@@ -10,7 +11,7 @@ const router: Router = express.Router();
 
 const trainingRepository = new TrainingRepository();
 
-@logError('Error occurred')
+// @logError('Error occurred')
 router.post('/training', requireAuth, (req: Request, res: Response) => {
   try {
     const { date, title, exercise } = req.body as { date: string; title: string; exercise: any[] };
@@ -24,6 +25,7 @@ router.post('/training', requireAuth, (req: Request, res: Response) => {
   }
 });
 
+// @get('/training')
 router.get('/training', requireAuth, (req: Request, res: Response) => {
   try {
     const { date } = req.body as { date: string };
