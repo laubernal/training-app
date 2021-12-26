2399d2443415ec259b6c9308bafe8e9d9c46e787
laubernal <lbernalrodrguez@gmail.com>
2021-12-26 21:14:37 +0100

    add docker compose file

--- src/Application/Dto/newTrainingRequestDto.ts
+++ src/Application/Dto/newTrainingRequestDto.ts
@@ -2,5 +2,5 @@ import { Exercise } from '../../Domain/entities/Exercise';
 import { Serie } from '../../Domain/entities/Serie';
 
 export class NewTrainingRequestDto {
-  constructor(public exercise: Exercise[], public exerciseName: string, public series: Serie[]) {}
+  constructor(public date: string, public title: string, public exercise: Exercise[], public exerciseName: string, public series: Serie[]) {}
 }
