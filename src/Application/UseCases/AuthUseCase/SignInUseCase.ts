fbbea53e18dd8eb4771d135d14922d35700e31c8
laubernal <lbernalrodrguez@gmail.com>
2022-01-13 11:19:24 +0100

    clean and test singin use case and controller

--- src/Application/UseCases/AuthUseCase/SignInUseCase.ts
+++ src/Application/UseCases/AuthUseCase/SignInUseCase.ts
@@ -1,35 +1,31 @@
 import { scryptSync } from 'crypto';
 
-import { IUserRepository } from '../../../Infrastructure/interfaces/IUserRepository';
 import { IUseCase } from '../IUseCase';
 import { Email } from '../../../Domain/vo/Email';
 import { IUserPgRepository } from '../../../Infrastructure/interfaces/PostgresqlDbInterfaces/IUserPgRepository';
 
 export class SignInUseCase implements IUseCase<void> {
-  // constructor(private userRepository: IUserRepository) {}
   constructor(private userPgRepository: IUserPgRepository) {}
 
   public async execute(email: string, password: string): Promise<void> {
     const emailValidated = new Email(email);
 
-    // const userExists = this.userRepository.getOneBy('email', emailValidated.value);
     const userExists = await this.userPgRepository.getOneBy('us_email', emailValidated.value);
 
     if (!userExists) {
       throw new Error('This user does not exist');
     }
 
-    // if (this.comparePasswords(userExists.password, password) === false) {
-    //   throw new Error('Incorrect password');
-    // }
+    if (this.comparePasswords(userExists.password, password) === false) {
+      throw new Error('Incorrect password');
+    }
   }
 
-  // private comparePasswords(saved: string, supplied: string): boolean {
-  //   const [hashed, salt] = saved.split('.');
-  //   console.log(hashed, salt);
+  private comparePasswords(saved: string, supplied: string): boolean {
+    const [hashed, salt] = saved.split('.');
 
-  //   const suppliedHashedBuf = scryptSync(supplied, salt, 64);
+    const suppliedHashedBuf = scryptSync(supplied, salt, 64);
 
-  //   return hashed === suppliedHashedBuf.toString('hex');
-  // }
+    return hashed === suppliedHashedBuf.toString('hex');
+  }
 }
--- src/Infrastructure/controllers/Auth/SignInController.ts
+++ src/Infrastructure/controllers/Auth/SignInController.ts
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken';
 
 import { SignInUseCase } from '../../../Application/UseCases/AuthUseCase/SignInUseCase';
 import { UserPgRepository } from '../../repositories/PostgresqlDb/UserPgRepository';
-import { UserRepository } from '../../repositories/UserRepository';
 import { bodyValidator, Controller, post } from '../decorators';
 
 @Controller()
@@ -14,40 +13,23 @@ export class SignInController {
     try {
       const { email, password } = req.body;
 
-      // const userRepository = new UserRepository();
       const userPgRepository = new UserPgRepository();
 
-      //PRUEBAS-------------------
-      // const user = await userPgRepository.getOneBy('us_id', '1');
-      // console.log('result', user);
-
-      // userPgRepository.save('us_id, us_first_name, us_last_name, us_email, us_password', [
-      //   '4',
-      //   'Demonitu',
-      //   'Demonil',
-      //   'demonitu@gmail.com',
-      //   'password',
-      // ]);
-
-      //FIN PRUEBAS---------------------
-
-      // new SignInUseCase(userRepository).execute(email, password);
       await new SignInUseCase(userPgRepository).execute(email, password);
 
-      // const id = userRepository.getId(email);
-      // const id = userPgRepository.getId(email);
+      const id = await userPgRepository.getId(email);
 
-      // const userJwt = jwt.sign(
-      //   {
-      //     id: id,
-      //     email: email,
-      //   },
-      //   process.env.TOKEN_KEY!
-      // );
+      const userJwt = jwt.sign(
+        {
+          id: id,
+          email: email,
+        },
+        process.env.TOKEN_KEY!
+      );
 
-      // req.session = {
-      //   jwt: userJwt,
-      // };
+      req.session = {
+        jwt: userJwt,
+      };
 
       res.status(201).send('Logged in');
     } catch (err: any) {
